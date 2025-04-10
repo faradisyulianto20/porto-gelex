ac324f8806ae9d60832ccf8cfa7b4bc7fa8c21d6
faradisyulianto20 <faradisy20@gmail.com>
2025-04-10 21:46:13 +0700

    download pdf

--- src/components/heroes/heroes.component.jsx
+++ src/components/heroes/heroes.component.jsx
@@ -3,6 +3,8 @@ import './heroes.styles.scss'
 import SocialMedia from '../social-media/socialmedia.component'
 import Iframe from '../iframe/iframe.component'
 
+import pdf from '../../assets/files/CV_Faradis_Yulianto.pdf'
+
 const Heroes = () => {
     return (
         <>
@@ -13,7 +15,7 @@ const Heroes = () => {
                         <p className='Heroes-paragraph'>I'm <strong>Faradis Yulianto</strong>, I am a <strong>Software Engineering</strong> student at <strong>Universitas Gadjah Mada</strong>, passionate about <strong>Front-End Development</strong>, <strong>UI/UX Design</strong>, <strong>Computer Vision</strong>, and <strong>Artificial Intelligence</strong>. I enjoy creating innovative and user-centric digital solutions.</p>
                         <p className='Heroes-hook'>Feel free to explore my work below, and don’t hesitate to reach out if you're interested in creating something amazing together!</p>
                         <div className='Heroes-Socialmedia'>
-                            <button className='Heroes-button'>Download CV</button>
+                        <a href={pdf} download='CV.pdf'><button className='Heroes-button'>Download CV</button></a>
                             <SocialMedia />
                         </div>
                     </div>
